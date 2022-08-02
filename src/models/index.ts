66127c65699ab50bb12b6d17a7bc315de553ea87
Ariel Rodrigues <arielrodrigues.dev@gmail.com>
2022-08-02 01:49:43 -0300

    adicionando schemas para export

--- src/models/index.ts
+++ src/models/index.ts
@@ -4,6 +4,11 @@ import estabelecimentoSchema from "./Estabelecimento";
 import enderecoSchema from "./Endereco";
 import horarioSchema from "./Horario";
 import imagesSchema from "./Images"
+import cardapioSchema from "./Cardapio";
+import categoriaSchema from "./Categoria";
+import subcategoriaSchema from "./Subcategoria";
+import produtoSchema from "./Produto";
+import pedidosSchema from "./Pedido";
 
 //const sintomas = new Sintomas(mySqlConection);
 const proprietario = new proprietarioSchema();
@@ -11,6 +16,11 @@ const estabelecimento = new estabelecimentoSchema();
 const endereco = new enderecoSchema();
 const horario = new horarioSchema();
 const images = new imagesSchema();
+const cardapio = new cardapioSchema();
+const categoria = new categoriaSchema();
+const subcategoria = new subcategoriaSchema();
+const produto = new produtoSchema();
+const pedido = new pedidosSchema();
 
 
 export {
@@ -19,4 +29,9 @@ export {
   endereco,
   horario,
   images,
+  cardapio,
+  categoria,
+  subcategoria,
+  produto,
+  pedido
 };
